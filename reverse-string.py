541a4e8748068331fb528186f4ab30aae1fe455f
SwanHtetAungX <contact.swanhtetaung@gmail.com>
2023-02-17 12:16:28 +0800

    solution to reverse the string

---
+++ reverse-string.py
@@ -0,0 +1,12 @@
+# Description
+# Complete the solution so that it reverses the string passed into it.
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+#My solution
+def solution(string):
+    '''To return the reversed string of the given string'''
+    return string[::-1]
+
+
+
